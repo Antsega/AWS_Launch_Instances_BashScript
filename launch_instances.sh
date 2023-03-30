5897dd51a74d2bacb9a39bc362ef69e460d008b6
antsega <astrowelding@gmail.com>
2023-03-30 12:28:19 -0500

    adding, pull IP for instances feature

--- launch_instances.sh
+++ launch_instances.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Test Test
-
 #Written by Anthony Segarra on 3/28/2023
+
+
 #usage: make script executable with chmod 744 script_name.sh
 #       to run from your terminal, type ./script_name.sh
 
@@ -13,15 +13,17 @@ then
     exit
 fi
 
-# Get instance IDs of all running instances
-INSTANCE_IDS=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[].Instances[].InstanceId" --output text)
+# Get instance IDs and Public IP addresses of all stopped instances
+STOPPED_INSTANCES=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[].Instances[].[InstanceId,PublicIpAddress]" --output text)
 
 # Start all stopped instances
-if [ -z "$INSTANCE_IDS" ]
+if [ -z "$STOPPED_INSTANCES" ]
 then
     echo "No stopped instances found."
 else
+    INSTANCE_IDS=$(echo "$STOPPED_INSTANCES" | awk '{print $1}')
+    PUBLIC_IPS=$(echo "$STOPPED_INSTANCES" | awk '{print $2}')
     aws ec2 start-instances --instance-ids $INSTANCE_IDS
     echo "Starting instances: $INSTANCE_IDS"
+    echo "Public IP addresses: $PUBLIC_IPS"
 fi
-
