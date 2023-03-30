27b9203580511d90485217fe7acaef4ba0113c7b
Antsega <astrowelding@gmail.com>
2023-03-30 12:53:12 -0500

    added timer for elapsted time

--- launch_instances.sh
+++ launch_instances.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #Written by Anthony Segarra on 3/28/2023
-#usage: make script executable with chmod 744 script_name.sh
-#       to run from your terminal, type ./script_name.sh
 
 # Check if aws cli is installed
 if ! command -v aws &> /dev/null
@@ -21,10 +19,19 @@ then
 else
     aws ec2 start-instances --instance-ids $INSTANCE_IDS
     echo "Starting instances: $INSTANCE_IDS"
+    # Counter for instances starting
+    echo -n "Waiting for instances to start: "
+    SECONDS=0
+    while [ "$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query "Reservations[].Instances[].State[].Name" --output text)" != "running running running" ]; do
+        sleep 1
+        echo -n "."
+    done
+    echo ""
+    # Elapsed time
+    ELAPSED_TIME=$(date -u -d@"$SECONDS" +"%T")
+    echo "Instances started in $ELAPSED_TIME"
+    # Display instance info
     for i in $INSTANCE_IDS; do
-        # Wait until the instance is running
-        aws ec2 wait instance-running --instance-ids $i
-
         # Get the name and Public IPv4 address of the instance
         NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$i" "Name=key,Values=Name" --query "Tags[*].Value" --output text)
         PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $i --query 'Reservations[].Instances[].PublicIpAddress' --output text)
